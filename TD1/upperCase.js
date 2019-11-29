0ac4ceec3de72a7e285e1b0617571c1b0c7340ad
Titouan-Joseph <titouanjoseph@gmail.com>
2019-11-29 13:35:27 +0100

    fait jusqu'a la Q5.2

---
+++ TD1/upperCase.js
@@ -0,0 +1,9 @@
+var name = ["pierre","paul","jack"]
+
+function upperCaseFirstLetter(array){
+    for (i=0; i < array.length; i++){
+        console.log(array[i].toUpperCase())
+    }
+}
+
+upperCaseFirstLetter(name)
